e30e420e4d1b9578a868e4b098cbd8c46fd8c0fb
ntrelat <ntrelat@actweb.fr>
2017-06-16 14:46:19 +0200

    intégration homepage

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -4,9 +4,4 @@
 'use strict';
 
 var $ = require('jquery');
-var Link = require('../_modules/link/link');
-
-$(function() {
-  new Link(); // Activate Link modules logic
-  console.log('Welcome to Yeogurt!');
-});
+require('bootstrap');
