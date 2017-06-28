dcd4fd9af7b0bbe772b154955ea78f3248b3e95f
ntrelat <ntrelat@actweb.fr>
2017-06-28 10:32:55 +0200

    fix - lien accordion

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -11,14 +11,17 @@ require('./tab-accordion/jquery.tabAccordion');
 
 $(document).ready(function () {
   $('a[href*="#"]:not([href="#"])').on('click', function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top - parseInt($('body').css('padding-top'))
-        }, 800);
-        return false;
+    if(!$(this).parents('.tab-accordion')){
+
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - parseInt($('body').css('padding-top'))
+          }, 800);
+          return false;
+        }
       }
     }
   });
